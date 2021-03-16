a85b6b7deb6c5d177e0721389c543aed674814df
hyl <10321970@qq.com>
2021-03-16 19:54:54 +0800

    created src/pages/202103/

---
+++ src/pages/202103/share1/index.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="container">
+        <div class="share1-top">
+            <div><img src="~@static/images/202103/share1/icon.png" alt="">闲聊</div>
+            <div>打开应用程序</div>
+        </div>
+        <div class="content">
+            <div>3月12日星期五 11:00</div>
+            <div>他们现在在哪里？2010年嘻哈电台</div>
+            <div>MIND BODY GAME</div>
+            <div><img src="~@static/images/202103/share1/more.png" alt=""></div>
+            <div>W/阿德里安·格莱特—让我们来回顾一下那发货的金凤凰的发货的设计费和会发生的借款方你发集宁师范发电放费 分开讲道理鬼哭狼嚎；</div>
+        </div>
+        <div class="hasacount">已有账号</div>
+        <div class="chakan">查看完整事件</div>
+        <div class="des">还没俱乐部账号？ 获取应用程序尽早访问吧</div>
+        <div class="appstore"><img src="~@static/images/202103/share1/appstore.png" alt=""></div>
+    </div>
+</template>
+
+<script>
+// import login from '@/components/login/index.vue'
+// import { getActivityExpress } from '@/api/201811/express'
+
+// import getParams from '@/utils/urlparams'
+// const params = getParams()
+
+export default {
+    name: 'index',
+    data () {
+        return {
+            
+        }
+    }
+};
+</script>
+
+<style src="./index.styl" lang="stylus"></style>
