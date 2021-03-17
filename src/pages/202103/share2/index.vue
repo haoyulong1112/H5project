633d6d3309e707ca934144bbb8bc2b4c953b9279
hyl <10321970@qq.com>
2021-03-17 19:05:17 +0800

    update src/pages/202103/share2/

---
+++ src/pages/202103/share2/index.vue
@@ -0,0 +1,49 @@
+<template>
+    <div class="container">
+        <div class="share1-top">
+            <div><img src="~@static/images/202103/share1/icon.png" alt="">闲聊</div>
+            <div>打开应用程序</div>
+        </div>
+        <div class="content">
+            <div>宇宙阳台<img src="~@static/images/202103/share1/clubicon.png" alt=""></div>
+            <div>🎵+🎨合作站（现场音乐+现场绘画）</div>
+            <div>
+                <div>
+                    <img src="~@static/images/202103/share1/more.png" alt="">
+                    <img src="~@static/images/202103/share1/more.png" alt="">
+                </div>
+                <div>
+                    <p>帆布鞋也能走天下<img src="~@static/images/202103/share1/lt1.png" alt=""></p>
+                    <p>︶ㄣ拽、爷ㄜ︵<img src="~@static/images/202103/share1/lt1.png" alt=""></p>
+                    <p>我是吃货我骄傲<img src="~@static/images/202103/share1/lt1.png" alt=""></p>
+                </div>
+            </div>
+            <div>
+                <span>437</span>
+                <img src="~@static/images/202103/share1/ren.png" alt="">/<span>17</span>
+                <img src="~@static/images/202103/share1/lt.png" alt="">
+            </div>
+        </div>
+        <div class="des">还没俱乐部账号？ 获取应用程序尽早访问吧</div>
+        <div class="appstore"><img src="~@static/images/202103/share1/appstore.png" alt=""></div>
+    </div>
+</template>
+
+<script>
+// import login from '@/components/login/index.vue'
+// import { getActivityExpress } from '@/api/201811/express'
+
+// import getParams from '@/utils/urlparams'
+// const params = getParams()
+
+export default {
+    name: 'index',
+    data () {
+        return {
+            
+        }
+    }
+};
+</script>
+
+<style src="./index.styl" lang="stylus"></style>
--- src/pages/202103/share3/index.vue
+++ src/pages/202103/share3/index.vue
@@ -4,14 +4,20 @@
             <div><img src="~@static/images/202103/share1/icon.png" alt="">闲聊</div>
             <div>打开应用程序</div>
         </div>
-        <div class="content">
-            <div>3月12日星期五 11:00</div>
-            <div>他们现在在哪里？2010年嘻哈电台</div>
-            <div>MIND BODY GAME<img src="~@static/images/202103/share1/clubicon.png" alt=""></div>
-            <div><img src="~@static/images/202103/share1/more.png" alt=""></div>
-            <div>W/阿德里安·格莱特—让我们来回顾一下那发货的金凤凰的发货的设计费和会发生的借款方你发集宁师范发电放费 分开讲道理鬼哭狼嚎；</div>
+        <div class="desbig">还没俱乐部账号？ 获取应用程序尽早访问吧</div>
+        <div class="download">
+            <div><img src="~@static/images/202103/share1/icon.png" alt=""></div>
+            <div>
+                <div>闲聊</div>
+                <div>社交网络</div>
+                <div><img src="~@static/images/202103/share1/xx.png" alt=""></div>
+            </div>
+            <div>下载</div>
         </div>
-        <div class="hasacount">已有账号</div>
+        <div class="status">
+            <div>这个房间已经结束</div>
+            <div>但是现在又哦更多的正在开启</div>
+        </div>
         <div class="chakan">查看完整事件</div>
         <div class="des">还没俱乐部账号？ 获取应用程序尽早访问吧</div>
         <div class="appstore"><img src="~@static/images/202103/share1/appstore.png" alt=""></div>
