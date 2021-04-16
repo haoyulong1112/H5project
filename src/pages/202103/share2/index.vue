ef7da0345861800e62bee324a0847be64a346816
haoyulong <haoyulong1112@163.com>
2021-04-16 17:50:19 +0800

    udpate package-lock.json

--- src/pages/202103/share2/index.vue
+++ src/pages/202103/share2/index.vue
@@ -40,7 +40,7 @@ export default {
     name: 'index',
     data () {
         return {
-            
+
         }
     }
 };
--- static/config/index.js
+++ static/config/index.js
@@ -1,4 +1,4 @@
-(function (window) {
+(function(window) {
     // 区分环境的配置 252 demo prod
     var testConfig = {
         // 请求域名头 pet-pai
@@ -11,7 +11,7 @@
         manageDomain: 'http://192.168.0.252'
     }
 
-    
+
     var demoConfig = {
         // 请求域名头 pet-pai
         requestDomain: 'http://8.141.49.230',
@@ -23,27 +23,27 @@
         manageDomain: 'http://8.141.49.230'
     }
 
-    var prodConfig = {
-        // 请求域名头 pet-pai
-        requestDomain: 'https://api.ichongwujia.com',
-        // 分享appid
-        shareAppid: '101940259',
-        // 分享域名头
-        shareDomain: 'http://static.ichongwujia.com',
-        // 管理域名头
-        manageDomain: 'http://manage.ichongwujia.com'
-    }
+    // var prodConfig = {
+    //     // 请求域名头 pet-pai
+    //     requestDomain: 'https://api.ichongwujia.com',
+    //     // 分享appid
+    //     shareAppid: '101940259',
+    //     // 分享域名头
+    //     shareDomain: 'http://static.ichongwujia.com',
+    //     // 管理域名头
+    //     manageDomain: 'http://manage.ichongwujia.com'
+    // }
 
     // 固定的配置
     var config = {
         // rem布局页面最大宽度
         pageWidth: 640,
         // 分享图片
-        shareImg: '~@static/icons/xianliao.png',
+        shareImg: '~@static/images/icons/xianliao.png',
         // favicon
-        favicon: '~@static/icons/xianliao.png',
+        favicon: '~@static/images/icons/xianliao.png',
         // 跳转应用宝下载APP
-        downloadApp: function () { window.location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.haotang.pet&g_f=991653' },
+        downloadApp: function() { window.location.href = 'http://a.app.qq.com/o/simple.jsp?pkgname=com.haotang.pet&g_f=991653' },
         // 下载组件logo
         downloadLogo: 'http://dev-pet-avatar.oss-cn-beijing.aliyuncs.com/html5/15655992124122270752.png',
         // 下载组件title
@@ -62,7 +62,7 @@
 
     var hostname = window.location.hostname
 
-    function isItemInStr (str, arr) {
+    function isItemInStr(str, arr) {
         for (var i = 0, len = arr.length; i < len; ++i) {
             if (str.indexOf(arr[i]) !== -1) { return true }
         }
